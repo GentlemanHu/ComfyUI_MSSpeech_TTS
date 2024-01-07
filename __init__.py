75e082dc87c7f3e2614fbf642007f68ff2ae317a
gentlemanhu_manjaro_hypland <justfeelingme@gmail.com>
2024-01-08 00:07:17 +0800

    update

--- __init__.py
+++ __init__.py
@@ -9,12 +9,13 @@ NODE_DISPLAY_NAME_MAPPINGS = {}
 
 if init():
     py = get_ext_dir("py")
-    files = glob.glob("*.py", root_dir=py, recursive=False)
+    files = glob.glob("*.py", root_dir=py, recursive=True)
     for file in files:
         name = os.path.splitext(file)[0]
         spec = importlib.util.spec_from_file_location(name, os.path.join(py, file))
         module = importlib.util.module_from_spec(spec)
         sys.modules[name] = module
+        print(module)
         spec.loader.exec_module(module)
         if hasattr(module, "NODE_CLASS_MAPPINGS") and getattr(module, "NODE_CLASS_MAPPINGS") is not None:
             NODE_CLASS_MAPPINGS.update(module.NODE_CLASS_MAPPINGS)
